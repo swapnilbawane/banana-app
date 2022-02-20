d1fd7b13539ea83a5a55b76ac8e29012b5acc05d
swapnilbawane <swapnilbawane08@gmail.com>
2022-02-20 15:17:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// EXAMPLE OF CONSOLE =: console.log("Hi from console");
+// SHORTCUT: Ctrl+/ adds comment sign. 
+// console.log("Welcome from the js file. ")
+
+// EXAMPLE OF PROMPT AND ALERT 
+// input 
+// var userName = prompt("What is your name?");
+
+// processing 
+// var welcomeMessage = "Hi "+userName;
+
+//output
+// alert(welcomeMessage);
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked") 
+})
